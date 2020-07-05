2d22b27eacb85bf4c641f5b1f9886cbceb32f1eb
Maciej <ostromaciej@gmail.com>
2020-07-05 02:56:52 +0200

    Updates

---
+++ precache-manifest.7745f975d320b42b2886447a1a280ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8518f7488531900425ba54b20b787e6c",
+    "url": "/scratchpad/index.html"
+  },
+  {
+    "revision": "2320e4588bfba80fd311",
+    "url": "/scratchpad/static/css/main.64694461.chunk.css"
+  },
+  {
+    "revision": "11cf025fb08fd84b07d5",
+    "url": "/scratchpad/static/js/2.2ef4e3a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scratchpad/static/js/2.2ef4e3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2320e4588bfba80fd311",
+    "url": "/scratchpad/static/js/main.2e3aeb66.chunk.js"
+  },
+  {
+    "revision": "02f850eb450445c8ed43",
+    "url": "/scratchpad/static/js/runtime-main.a4b968f7.js"
+  }
+]);
